a979d3fac86037d26c23a33526f6bf2fae675e3e
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2020-05-25 16:06:57 -0400

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Neftali Samarey on 5/25/20.
+//  Copyright © 2020 Neftali Samarey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
